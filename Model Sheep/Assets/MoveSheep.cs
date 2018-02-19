f00011add2212335e304d85728861e1c25e9946d
GeorgeEvans <psxge1@exmail.nottingham.ac.uk>
2018-02-19 11:49:01 +0000

    Sheep v1
    First upload of basic moving sheep.

---
+++ Model Sheep/Assets/MoveSheep.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSheep : MonoBehaviour
+{
+
+    float speed = 7.0f;
+
+    void Update()
+    {
+        var move = new Vector3(0, 0, Input.GetAxis("Vertical"));
+
+        transform.Rotate( 0.0f, -Input.GetAxis("Horizontal")* speed,  0.0f);
+
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
