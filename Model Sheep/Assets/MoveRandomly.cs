8650e622e69abfbb6ee949a4b29192570e002349
GeorgeEvans <psxge1@exmail.nottingham.ac.uk>
2018-03-12 11:35:25 +0000

    Sheep V3.2
    Code has been refactored for sheep movement

--- Model Sheep/Assets/MoveRandomly.cs
+++ Model Sheep/Assets/MoveRandomly.cs
@@ -5,69 +5,71 @@ using UnityEngine.AI;
 
 public class MoveRandomly : MonoBehaviour {
 
-    public float timer;
 
-    public int newtarget;
+	public float timer;         // Used to update time in the code.
 
-    public float speed;
+	public int newtarget;       // Used for picking the direction of a model, which is updated according to the time.
 
-    public NavMeshAgent nav;
+	public float speed;		// Sets the speed of the Model.
 
-    public Vector3 Target;
+	public NavMeshAgent nav;	// The NavMesh allows the model to travel.
 
-    public float jumpheight = 50000000;
+	public Vector3 Target;	// Gives the Model a movement to reach newtarget.
 
-    public float jumpTime;
-    
+	public float jumpheight = 50000000;	// Variable used to give height to the model.
+
+	public float jumpTime;	// Calculates how often the model should jump.
+
 	// Use this for initialization
 	void Start () {
 
-        nav = gameObject.GetComponent<NavMeshAgent>();
-		
+		nav = gameObject.GetComponent<NavMeshAgent>();
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-        timer += Time.deltaTime;
-        jumpTime += Time.deltaTime;
+		timer += Time.deltaTime;
+		jumpTime += Time.deltaTime;		// Time related variables are updated.
 
-        if(timer <= newtarget)
-        {
-            newTarget();
-            timer = 0;
-        }
+		if(timer <= newtarget)
+		{
+			newTarget();
+			timer = 0;				// Picks the next target, resets the timer.
 
-        if(jumpTime <= newtarget)
-        {
-            Jump();
+		}
 
-            jumpTime = 0;
-        }
-        nav.speed = speed;
+		if(jumpTime <= newtarget)
+		{
+			Jump();
 
-       
+			jumpTime = 0;			// forces a jump, resets the timer.
+		}
+		nav.speed = speed;
+
+
 
 	}
 
-    void Jump ()
-    {
-        GetComponent<Rigidbody>().AddForce(0, jumpheight, 0);
-    }
+	void Jump ()				// Code used to force a jump. 
+	{
+		GetComponent<Rigidbody>().AddForce(0, jumpheight, 0);
+	}
 
-    void newTarget ()
-    {
-        float myX = gameObject.transform.position.x;
-        float myZ = gameObject.transform.position.z;
+	void newTarget ()				// Code used to change target.
+	{
+		float myX = gameObject.transform.position.x;
+		float myZ = gameObject.transform.position.z;
 
-        float xPos = myX + Random.Range(myX - 100, myX + 100);
-        float zPos = myZ + Random.Range(myZ - 100, myZ + 100);
+		float xPos = myX + Random.Range(myX - 100, myX + 100);
+		float zPos = myZ + Random.Range(myZ - 100, myZ + 100);
 
-        Target = new Vector3(xPos, gameObject.transform.position.y, zPos);
+		Target = new Vector3(xPos, gameObject.transform.position.y, zPos);
 
-        nav.SetDestination(Target);
-       
-    }
+		nav.SetDestination(Target);
+
+	}
 
 
 }
