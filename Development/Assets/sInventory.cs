3ea98bf0f8bf4eb33e01ab97ce18c4899e99c5f5
GeorgeEvans <psxge1@exmail.nottingham.ac.uk>
2018-03-31 13:08:15 +0100

    Menu button fix
    The emnu bitton "i" now works as before.

---
+++ Development/Assets/sInventory.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sInventory : MonoBehaviour {
+
+	public GameObject Menu; // Assign in inspector
+	public GameObject Crosshair;
+	public GameObject TimerText;
+	private bool isShowing;
+
+	void Start(){
+
+		isShowing = false;
+		Menu.SetActive(isShowing);
+	}
+
+
+
+	void Update() {
+		if (Input.GetKeyDown("i")) {
+			isShowing = !isShowing;
+			Menu.SetActive(isShowing);
+			isShowing = !isShowing;
+			Crosshair.SetActive(isShowing);
+			TimerText.SetActive (isShowing);
+			isShowing = !isShowing;
+		}
+	}
+}
