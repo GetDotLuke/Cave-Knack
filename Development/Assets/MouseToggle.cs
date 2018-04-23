965282e65f407f471d16d698eb76b0c34e7047c0
GeorgeEvans <psxge1@exmail.nottingham.ac.uk>
2018-04-23 11:59:31 +0100

    Mouse Toggle
    Mouse toggle option available when in menu mode in order to select the tools.

---
+++ Development/Assets/MouseToggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseToggle : MonoBehaviour {
+
+	bool cursorLocked = true;
+
+	void Update () {
+		if (Input.GetKeyDown ("i") ){
+			cursorLocked = !cursorLocked;
+		}
+		if (cursorLocked) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = true;
+			cursorLocked = false;
+		} else {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+			cursorLocked = true;
+		}
+
+	}
+
+}
